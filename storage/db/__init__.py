6956ca54ecdf3d4749e87015bd339b0574afff3b
fengl <flfeng000@gmail.com>
2024-12-20 10:07:16 +0800

    feat: add async db connector && fix session_ctx()

--- storage/db/__init__.py
+++ storage/db/__init__.py
@@ -1,8 +1,15 @@
-from contextlib import contextmanager
+from contextlib import asynccontextmanager, contextmanager
 from contextvars import ContextVar, Token
+from enum import Enum
 from uuid import uuid4
 
 from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_scoped_session,
+    async_sessionmaker,
+    create_async_engine,
+)
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 from config import settings
@@ -24,6 +31,16 @@ def reset_session_id(token: Token):
     _request_id_ctx_var.reset(token)
 
 
+class IsolationLevel(Enum):
+    READ_UNCOMMITTED = "READ UNCOMMITTED"
+    READ_COMMITTED = "READ COMMITTED"
+    REPEATABLE_READ = "REPEATABLE READ"
+    SERIALIZABLE = "SERIALIZABLE"
+
+    def __str__(self):
+        return self.value
+
+
 class DatabaseConnector:
     def __init__(self):
         self.engine = create_engine(
@@ -34,18 +51,83 @@ class DatabaseConnector:
             pool_recycle=3600,
             pool_pre_ping=True,
         )
-
-        factory = sessionmaker(bind=self.engine)
-        self.session = scoped_session(factory, get_session_id)
+        self.session = scoped_session(
+            sessionmaker(bind=self.engine),
+            get_session_id,
+        )
 
     @contextmanager
-    def session_ctx(self):
-        token = set_session_id(uuid4().hex)
+    def session_ctx(
+        self,
+        isolation_level=IsolationLevel.READ_COMMITTED,
+    ):
+        token = set_session_id(str(uuid4()))
 
-        yield self.session
+        self.session.connection(
+            execution_options={"isolation_level": str(isolation_level)}
+        )
 
-        self.session.remove()
-        reset_session_id(token)
+        err = None
+        try:
+            yield self.session()
+        except Exception as e:
+            err = e
+        finally:
+            self.session.remove()
+            reset_session_id(token)
+            if err:
+                raise err
+
+
+class AsyncDatabaseConnector:
+    def __init__(self):
+        # 创建异步引擎
+        self.engine = create_async_engine(
+            settings.DATABASE_URL,
+            pool_size=50,
+            max_overflow=50,
+            pool_timeout=10,
+            pool_recycle=1800,
+            pool_pre_ping=True,
+        )
+        self.session = async_scoped_session(
+            async_sessionmaker(
+                self.engine,
+                class_=AsyncSession,
+                autoflush=True,
+                expire_on_commit=False,
+            ),
+            scopefunc=get_session_id,
+        )
+
+    async def close(self):
+        """
+        关闭数据库引擎
+        """
+        await self.engine.dispose()
+
+    @asynccontextmanager
+    async def session_ctx(
+        self,
+        isolation_level=IsolationLevel.READ_COMMITTED,
+    ):
+        token = set_session_id(str(uuid4))
+
+        await self.session.connection(
+            execution_options={"isolation_level": str(isolation_level)}
+        )
+
+        err = None
+        try:
+            yield self.session()
+        except Exception as e:
+            err = e
+        finally:
+            await self.session.remove()
+            reset_session_id(token)
+            if err:
+                raise err
 
 
 db = DatabaseConnector()
+# db = AsyncDatabaseConnector()
